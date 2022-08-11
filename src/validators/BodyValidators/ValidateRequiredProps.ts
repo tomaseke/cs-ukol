7da3aae9fb001935a745814d2d696636e1f9b357
TomášErben <tomas.erben@epptec.eu>
2022-08-11 15:46:52 +0200

    first task somewhat done

--- src/validators/BodyValidators/ValidateRequiredProps.ts
+++ src/validators/BodyValidators/ValidateRequiredProps.ts
@@ -12,7 +12,7 @@ export class ValidateRequiredProps{
                 throw new HttpException(400, `Property ${entry[0]} is missing!`)
             }
             if(typeof inputtedCurrency[entry[0]] !== entry[1]) {
-                throw new HttpException(400, `Property ${entry[0]} has incorrect type!`)
+                throw new HttpException(400, `Property ${entry[0]} should be of type ${entry[1]}!`)
             }
         }
     }
